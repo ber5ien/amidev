38af5fd8bf792e872b2c0085d8b0b7feb7ab4195
Rafal Zdziech <raf@amidev.co.uk>
2011-10-10 12:37:51 +0100

    Email sending added

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'net/smtp'
 
 # Helpers
 require './lib/render_partial'
+
+#send email
+def send_email(to,opts={})
+  opts[:server]      ||= 'localhost'
+  opts[:from]        ||= 'email@example.com'
+  opts[:subject]     ||= "You need to see this"
+  opts[:body]        ||= "Important stuff!"
+
+  msg = <<END_OF_MESSAGE
+From: #{opts[:from_alias]} <#{opts[:from]}>
+To: <#{to}>
+Subject: #{opts[:subject]}
+
+#{opts[:body]}
+END_OF_MESSAGE
+
+  Net::SMTP.start(opts[:server]) do |smtp|
+    smtp.send_message msg, opts[:from], to
+  end
+end
+
 
 # Set Sinatra variables
 set :app_file, __FILE__
@@ -30,6 +52,7 @@ post '/contact' do
   @email = params[:email]
   @subject = params[:subject]
   @email_message = params[:email_message]
+  send_email "raf@amidev.co.uk", :from => @email, :subject => @subject, :body => @email_message
 
   haml :contact, :layout => :'layouts/page'
 
